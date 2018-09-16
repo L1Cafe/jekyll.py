11140403ac4d8e901e372346e42abb43810ef886
L1Cafe <L-Cafe-github@tuta.io>
2018-09-16 19:43:14 +0200

    a
    a

---
+++ pyrite.py
@@ -0,0 +1,5 @@
+import sys
+
+title = input("Enter the post title:")
+tags = input("Enter the post tags, separated by spaces or nothing for no tags:")
+categories = input("Enter the post categories, separated by spaces or nothing for no categories:")
