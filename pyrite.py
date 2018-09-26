cbc13c13c1d7107d72ad37241b37313e395feaa3
L1Cafe <L-Cafe-github@tuta.io>
2018-09-26 12:01:37 +0200

    needs obvious cleanup

--- pyrite.py
+++ pyrite.py
@@ -5,15 +5,20 @@ import datetime
 title = input("Enter the post title: ")
 filename_title = input("Enter the filename, separate words by '-' : ")
 tags = input("Enter the post tags, separated by spaces or nothing for no tags: ")
-category = input("Enter the post categories, separated by spaces or nothing for no categories: ")
+category = input("Enter the post category, or nothing for no categories: ")
 
 title_arr = title.split(' ')
 
 filename = str(datetime.date.today()) + '-' + filename_title
 
+os.makedirs(os.path.join('assets', 'images', filename)) # TODO: PROPER ERROR HANDLING
+os.makedirs(os.path.join('_posts')) # TODO: PROPER ERROR HANDLING
+
 with open(os.path.join('_posts', filename + '.markdown'), 'w') as f:
 	f.write('---\ntitle:\t\t"' + title + '"\ndate:\t\t')
-	f.write(str(datetime.date.today()) + '\ntags:\t\t' + tags + '\n')
+	f.write(str(datetime.date.today()))
+	if tags:
+		f.write('\ntags:\t\t' + tags + '\n')
 	if category:
 		f.write('category:\t' + category + '\n')
 	f.write('comment:\theader sizes are 2000x750\nheader:\n\timage:\t\t')
@@ -24,4 +29,3 @@ with open(os.path.join('_posts', filename + '.markdown'), 'w') as f:
 	f.write('"Photo credit: [**Unsplash**](https://unsplash.com)"\n---\n\n')
 	f.write('Start writing here.\n')
 
-os.makedirs(os.path.join('assets', 'images', filename)) # TODO: PROPER ERROR HANDLING
