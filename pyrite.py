87133808b6b9954f51ae912b4c23e1163a7d8764
L1Cafe <L-Cafe-github@tuta.io>
2018-11-17 11:51:27 +0100

    update

--- pyrite.py
+++ pyrite.py
@@ -4,6 +4,7 @@ import datetime
 
 title = input("Enter the post title: ")
 filename_title = input("Enter the filename, separate words by '-' : ")
+# TODO: Show categories and tags
 tags = input("Enter the post tags, separated by spaces or nothing for no tags: ")
 category = input("Enter the post category, or nothing for no categories: ")
 
@@ -12,20 +13,20 @@ title_arr = title.split(' ')
 filename = str(datetime.date.today()) + '-' + filename_title
 
 os.makedirs(os.path.join('assets', 'images', filename)) # TODO: PROPER ERROR HANDLING
-os.makedirs(os.path.join('_posts')) # TODO: PROPER ERROR HANDLING
+#os.makedirs(os.path.join('_posts')) # TODO: PROPER ERROR HANDLING
 
 with open(os.path.join('_posts', filename + '.markdown'), 'w') as f:
-	f.write('---\ntitle:\t\t"' + title + '"\ndate:\t\t')
+	f.write('---\ntitle:    "' + title + '"\ndate:     ')
 	f.write(str(datetime.date.today()))
 	if tags:
-		f.write('\ntags:\t\t' + tags + '\n')
+		f.write('\ntags:     ' + tags + '\n')
 	if category:
 		f.write('category:\t' + category + '\n')
-	f.write('comment:\theader sizes are 2000x750\nheader:\n\timage:\t\t')
+	f.write('comment:  header sizes are 2000x750\nheader:\n  image:    ')
 	f.write(str(os.path.join('assets', 'images', filename, 'header.jpg')))
-	f.write('\n\tteaser:\t\t')
+	f.write('\n  teaser:   ')
 	f.write(str(os.path.join('assets', 'images', filename, 'header.jpg')))
-	f.write('\n\tcaption\t\t')
+	f.write('\n  caption:  ')
 	f.write('"Photo credit: [**Unsplash**](https://unsplash.com)"\n---\n\n')
 	f.write('Start writing here.\n')
 
